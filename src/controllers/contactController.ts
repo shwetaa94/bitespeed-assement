206e628591f92c836bb196299972ef559ee5f9ed
shweta <shwetasinghh9416@gmail.com>
2024-05-22 20:51:28 +0530

    new user logic added

--- src/controllers/contactController.ts
+++ src/controllers/contactController.ts
@@ -2,5 +2,41 @@ import { Request, Response } from 'express';
 import prisma from '../prisma';
 
 export const identifyContact = async (req: Request, res: Response) => {
- return "hello"
+  const { email, phoneNumber } = req.body;
+
+  try {
+    const existingContacts = await prisma.contact.findMany({
+      where: {
+        OR: [
+          { email: email },
+          { phoneNumber: phoneNumber }
+        ]
+      }
+    });
+
+     if(!existingContacts){
+      const newContact = await prisma.contact.create({
+        data: {
+          email: email,
+          phoneNumber: phoneNumber,
+          linkPrecedence: 'primary'
+        }
+      });
+
+      res.json({
+        contact: {
+          primaryContatctId: newContact.id,
+          emails: [newContact.email],
+          phoneNumbers: [newContact.phoneNumber],
+          secondaryContactIds: []
+        }
+      });
+    }
+    else {
+
+    }
+  } catch (err) {
+    console.error('Error identifying contact:', err);
+    res.status(500).send('Internal Server Error');
+  }
 };
