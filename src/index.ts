c55257d222ffa3ff1768a88c5415b8cf136c6b35
shweta <shwetasinghh9416@gmail.com>
2024-05-22 23:11:20 +0530

    final commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import express from 'express';
 import router from './routes/route';
+import { Request, Response } from "express";
+import dotenv from 'dotenv'
+dotenv.config();
 
 const app = express();
-const port = 8000;
-
+const port:any = process.env.PORT || 8000;
 app.use(express.json());
+
+
+app.get('/',(req: Request, res: Response)=>{
+    res.send("hello i am Identity Reconciliation")
+})
+
+//router call
 app.use('/', router);
+    
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
